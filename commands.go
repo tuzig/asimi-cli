0cc00a9199428b5efe379dba30300240e1f7907a
Benny Daon <benny@asimi.dev>
2026-01-06 12:43:41 +0200

    fix: simplifying Justfile creation on init

--- commands.go
+++ commands.go
@@ -21,6 +21,9 @@ var compactPrompt string
 //go:embed dotagents/sandbox/bashrc
 var sandboxBashrc string
 
+//go:embed dotagents/Justfile
+var defaultJustfile string
+
 // InitTemplateData holds data for the initialization prompt template
 type InitTemplateData struct {
 	ProjectName  string
